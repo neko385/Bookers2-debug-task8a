9f0d2cc84c19c0254ccd24ff98ace52f44e77a7a
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-01 04:07:58 +0000

    [Fix]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def index
     to = Time.current.at_end_of_day
     from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).sort_by {|x| x.favorited_users.where(favorites: {created_at: from...to}).size }.reverse
+    @books = Book.all.sort {|a,b| b.favorites.where(created_at: from...to).size <=> a.favorites.where(created_at: from...to).size }
     @book = Book.new
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :books,dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
-  has_many :favorited_books, through: :favorites, source: :book
   has_many :book_comments, dependent: :destroy
 
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
